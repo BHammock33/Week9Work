14b2abe2f60e3b8cbaab5c86a832bd3df690d2c7
BHammock33 <105878596+BHammock33@users.noreply.github.com>
2022-09-23 13:06:58 -0400

    working

--- src/main/java/com/coderscampus/myapp/main/App.java
+++ src/main/java/com/coderscampus/myapp/main/App.java
@@ -1,23 +1,54 @@
 package com.coderscampus.myapp.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.coderscampus.myapp.service.MyService;
 
 public class App {
-	
+
 	private MyService myService = new MyService();
-	
+
 	public static void main(String[] args) {
 		new App().execute();
 	}
 
 	private void execute() {
-		// Your code goes here, not in the main() method
-		// This removes any need to consume statics
-		// The code below is to be deleted once you understand what it does
-		System.out.println(AppConstants.EXAMPLE_CONSTANT);
-		System.out.println(" 2 * 17 = " + myService.multiply(2, 17));
-		System.out.println("Contents of the file at " + AppConstants.EXAMPLE_FILE_PATH);
-		System.out.println(myService.fileContents(AppConstants.EXAMPLE_FILE_PATH));
+
+		String[] names = new String[4];
+
+		names[0] = "Tervor";
+		names[1] = "Jane";
+		names[2] = "John";
+		names[3] = "Elon";
+
+		names[1] = null;
+
+		for (String name : names) {
+			System.out.println(name);
+		}
+
+		// collections java solution to data structure problems
+		// list is a type of collection, set is another, map as well
+		System.out.println("---------");
+		List<String> elements = new ArrayList<String>();
+		elements.add("Bennett Hammock");
+		elements.add("Maddie Hammock");
+		elements.add("Simon Hammock");
+		elements.add("Kyle Whitename");
+		elements.add("Elon Musk");
+
+		elements.remove(1);
+		printArrayToConsole(elements);
+		elements.remove("Elon Musk");
+		printArrayToConsole(elements);
+		
 	}
 
+	private void printArrayToConsole(List<String> elements) {
+		for (String element : elements) {
+			System.out.println(element);
+		}
+		System.out.println("--------");
+	}
 }
