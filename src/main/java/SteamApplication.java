ff1e7d3c0954d13cf096e6b555b250e6360d9698
BHammock33 <105878596+BHammock33@users.noreply.github.com>
2022-09-28 11:28:37 -0400

    mapStreaming

--- src/main/java/SteamApplication.java
+++ src/main/java/SteamApplication.java
@@ -1,10 +1,44 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SteamApplication {
 
 	public static void main(String[] args) {
 		
+		List<Car> cars = new ArrayList<>();
+		
+		cars.add(new Car("Tesla", "Model S", 2019));
+		cars.add(new Car("Tesla", "Model S", 2018));
+		cars.add(new Car("Tesla", "Model X", 2016));
+		cars.add(new Car("Tesla", "Model 3", 2019));
+		cars.add(new Car("Ford", "Maverick", 2021));
+		cars.add(new Car("Toyota", "Corrolla", 1997));
+		cars.add(new Car("Toyota", "Camery", 2008));
+		cars.add(new Car("Lambourghini", "Adventador", 2022));
+		
+		List<String> models = cars.stream()
+								  .map(car -> car.getModel())
+								  .collect(Collectors.toList());
+		
+		Set<String> brands = cars.stream()
+								  .map(car -> car.getBrand())
+								  .collect(Collectors.toSet());
+		
+		models.stream()
+			  .forEach(model -> System.out.println(model));
+		
+		System.out.println("----------");
+		
+		brands.stream()
+			  .forEach(brand -> System.out.println(brand));
+		
+		
+		//example1();
+	}
+
+	private static void example1() {
 		List<String> names = new ArrayList<>();
 		
 		populateNames(names);
@@ -13,7 +47,7 @@ public class SteamApplication {
 //			System.out.println(name);
 //		}
 		
-		names.parallelStream()
+		names.stream()
 			.forEach(name -> System.out.println(name));
 	}
 
